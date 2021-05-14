5c7c3b492bf3ab9bcaaee447b3e83613237059b4
Ubuntu <ubuntu@ip-172-31-4-43.us-west-2.compute.internal>
2021-05-14 05:32:16 +0000

    added redis test file

--- software/nodejs/redis.js
+++ software/nodejs/redis.js
@@ -2,4 +2,30 @@ const redis = require("redis");
 
 const client = redis.createClient();
 
+const subscriber = redis.createClient();
+const publisher = redis.createClient();
 
+let messageCount = 0;
+
+subscriber.on("subscribe", function(channel, count) {
+  // publish {"patient_id": "1234", "input_file": "hard/drive/path/to/zip"}
+  publisher.publish("a channel", "a message");
+  publisher.publish("a channel", "another message");
+});
+
+subscriber.on("message", function(channel, message) {
+  messageCount += 1;
+
+  console.log("Subscriber received message in channel '" + channel + "': " + message);
+  var jsonMsg = JSON.parse(message)
+  console.log(jsonMsg["patient_id"])
+  console.log(jsonMsg["processed_file"])
+
+  if (messageCount === 10) {
+    subscriber.unsubscribe();
+    subscriber.quit();
+    publisher.quit();
+  }
+});
+
+subscriber.subscribe("seg-results");
