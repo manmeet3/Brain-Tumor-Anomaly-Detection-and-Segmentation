1f81e7fc21e8feaca1ed42c063ee74d3764ad3cd
Manmeet Singh <manmeet3@gmail.com>
2021-05-15 12:40:10 -0700

    Update setup.sh

--- unet_3d_segmentation/setup.sh
+++ unet_3d_segmentation/setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script helps set up the environment to run this code on top of tensorflow14-gpu container from official docker repository
+# docker pull tensorflow/tensorflow:1.4.0-gpu
+
 pip install nibabel
 pip install SimpleITK
 pip install NiftyNet==0.2.0
@@ -7,4 +10,3 @@ pip install flask
 pip install redis
 apt update
 apt install redis-tools
-
